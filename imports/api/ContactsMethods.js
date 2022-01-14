a9f02d3fae05412e5277beaf4ccf7582388d2740
Eric Cavalcanti <ecavalcanti@gmail.com>
2022-01-14 18:36:05 -0300

    Implemented meteor methods with Optmistic UI

---
+++ imports/api/ContactsMethods.js
@@ -0,0 +1,19 @@
+import { ContactsCollection } from "./ContactsCollection";
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+
+Meteor.methods({
+  'contacts.insert'({ name, email, imageUrl }) {
+    check(name, String);
+    check(email, String);
+    check(imageUrl, String);
+    if (!name) {
+      throw new Meteor.Error('Name is required.');
+    }
+    return ContactsCollection.insert({ name, email, imageUrl, createdAt: new Date() });
+  },
+  'contacts.remove'({ contactId }) {
+    check(contactId, String);
+    return ContactsCollection.remove(contactId);
+  }
+});
